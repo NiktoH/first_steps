6faf8e761f9e89298113b697735cfc01ccee8174
NiktoH <sadretdinovs40@gmail.com>
2024-10-17 19:13:57 +0400

    Добавил новый класс с property

--- classMod_dumbclonesstarwars.py
+++ classMod_dumbclonesstarwars.py
@@ -13,6 +13,9 @@ class Shape(ABC):
     def perimetr(self) -> Union[int, float]:
         pass
 
+    def volume(self) -> Union[int, float]:
+        pass
+
 
 class Rectangle(Shape):
     _count: int = 0
@@ -24,7 +27,7 @@ class Rectangle(Shape):
         Rectangle._count += 1
 
     @staticmethod
-    def get_count(self) -> int:
+    def get_count() -> int:
         return Rectangle._count
 
     def area(self) -> Union[int, float]:
@@ -33,14 +36,33 @@ class Rectangle(Shape):
     def perimetr(self) -> Union[int, float]:
         return 2 * (self._width + self._height)
 
-    count = property(get_count)
-    sarea = property(area)
-    sperimetr = property(perimetr)
+
+class ThirdDimensionRectangle(Shape):
+    def __init__(self, width: Union[int, float],
+                height: Union[int, float],
+                length: Union[int, float]):
+        self._width: Union[int, float] = width
+        self._height: Union[int, float] = height
+        self._length: Union[int, float] = length
+
+    def area(self) -> Union[int, float]:
+        return 2 * (self._width * self._height + self._height * self._length + self._width * self._length)
+
+    def perimetr(self) -> Union[int, float]:
+        return 2 * (self._width + self._height)
+
+    @property
+    def volume(self):
+        return self._length * self._width * self._height
+
+
+r = ThirdDimensionRectangle(10, 10, 10)
 
 
 class Square(Rectangle):
     def __init__(self, side: Union[int, float]):
         super().__init__(side, side)
+
 
 
 class Circle(Shape):
@@ -51,7 +73,7 @@ class Circle(Shape):
         Circle._count += 1
 
     @staticmethod
-    def get_count(self):
+    def get_count() -> int:
         return Circle._count
 
     def area(self):
@@ -60,23 +82,21 @@ class Circle(Shape):
     def perimetr(self):
         return 2 * math.pi * self._radius
 
-    count = property(get_count)
-    sarea = property(area)
-    sperimetr = property(perimetr)
-
 
 if __name__ == "__main__":
     shapes = [
         Rectangle(3, 4),
         Square(3),
         Circle(2),
-        Rectangle(6, 7)
+        Rectangle(6, 7),
+        ThirdDimensionRectangle(10, 10, 10)
     ]
 
 
     for shape in shapes:
-        print(f"Фигура: {type(shape).__name__}, Площадь: {shape.sarea}, Периметр: {shape.sperimetr}")
+        print(f"Фигура: {type(shape).__name__}, Площадь: {shape.area()}, Периметр: {shape.perimetr()}")
 
 
-    print(f"Всего прямоугольников: {shape.count}")
-    print(f"Всего кругов: {shape.count}")
+    print(f"Всего прямоугольников: {Rectangle.get_count()}")
+    print(f"Всего кругов: {Circle.get_count()}")
+    print(f"Объём параллепипеда: {r.volume}")
