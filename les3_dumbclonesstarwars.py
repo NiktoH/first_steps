1754249c1b6fa05455d96b57ed92ec7ad6a8134a
NiktoH <sadretdinovs40@gmail.com>
2024-10-21 18:06:06 +0400

    Добавили аннотацию типов и метод записи

--- les3_dumbclonesstarwars.py
+++ les3_dumbclonesstarwars.py
@@ -1,15 +1,15 @@
 import json
 import csv
-
+from io import TextIOWrapper
 
 with open("in.json", "r") as in_file:
-    data = json.load(in_file)
+    data: str = json.load(in_file)
 
 
-def check_gen(data):
+def filtration(data) -> str:
     for entry in data:
-        phone = entry.get('phoneNumber', '')
-        user_agent = entry.get('userAgent', '')
+        phone: str = entry.get('phoneNumber', '')
+        user_agent: str = entry.get('userAgent', '')
         if (phone.startswith('+1') or phone.startswith('1')) and '4.0 Safari' in user_agent:
             yield {
                 'name': entry.get('name', ''),
@@ -18,10 +18,15 @@ def check_gen(data):
             }
 
 
-with open("out.csv", "w", newline='') as out_file:
-    fieldnames = ['name', 'address', 'email']
-    writer = csv.DictWriter(out_file, fieldnames=fieldnames)
+def writer() -> str:
+    with open("out.csv", "w", newline='') as out_file:
+        fieldnames: list[str] = ['name', 'address', 'email']
+        writer: str = csv.DictWriter(out_file, fieldnames=fieldnames)
 
-    writer.writeheader()
-    for item in check_gen(data):
-        writer.writerow(item)
+        writer.writeheader()
+        for item in filtration(data):
+            writer.writerow(item)
+
+
+filtration(data)
+writer()
